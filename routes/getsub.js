99a5ec766ca624bcd0801790d94d2c796d20769c
bipuldey19 <bipuldey19@gmail.com>
2023-09-13 05:44:12 +0600

    Added my own Imdb API 🛠️

--- routes/getsub.js
+++ routes/getsub.js
@@ -1,5 +1,8 @@
 const cheerio = require("cheerio");
-const { gotScraping } = require("got-scraping");
+const axios = require("axios");
+const {
+  gotScraping
+} = require("got-scraping");
 
 let express = require("express");
 let router = express.Router();
@@ -13,25 +16,27 @@ router.get("/subtitles/:path", async (req, res) => {
       throw new Error("Path Is Empty");
     }
     const url = `${baseUrl}/subtitles/${path}`;
-    const { body } = await gotScraping(url);
+    const {
+      body
+    } = await gotScraping(url);
     const $ = cheerio.load(body);
 
     let results = {};
 
     let title =
       $(".top.left .header h2")
-        .contents()
-        .filter(function () {
-          return this.nodeType === 3;
-        })
-        .text()
-        .trim() || null;
+      .contents()
+      .filter(function () {
+        return this.nodeType === 3;
+      })
+      .text()
+      .trim() || null;
 
     let year =
       $(".top.left .header ul li:first-child")
-        .text()
-        .replace("Year:", "")
-        .trim() || null;
+      .text()
+      .replace("Year:", "")
+      .trim() || null;
 
     let poster = null;
     const posterDiv = $(".top.left .poster");
@@ -41,64 +46,79 @@ router.get("/subtitles/:path", async (req, res) => {
     }
 
     let imdb = $("a.imdb").attr("href") || null;
-    let imdbInfo = {};
-    if (imdb) {
-      const imdbPage = await gotScraping(imdb);
-      const imdbHtml = imdbPage.body;
-      const imdb$ = cheerio.load(imdbHtml);
+    const parts = imdb.split('/');
+    const imdbId = parts[parts.length - 1];
 
-      const imdbDescription =
-        imdb$("p[data-testid='plot'] span:first-child").text().trim() || null;
-      const imdbRating =
-        imdb$("div[data-testid='hero-rating-bar__aggregate-rating__score']")
-          .first()
-          .text()
-          .split("/")[0]
-          .trim() || null;
-      const imdbRuntime =
-        imdb$("div h1[data-testid='hero__pageTitle']")
-          .closest("div")
-          .find("ul li:last-child")
-          .first()
-          .text()
-          .trim() || null;
-      const imdbGenres = [];
-      imdb$("div[data-testid='genres'] div:nth-child(2) a span").each(
-        (i, span) => {
-          imdbGenres.push(imdb$(span).text().trim());
-        }
-      );
-      const imdbGenresString = imdbGenres.join(", ").toString();
-      const imdbDirector =
-        imdb$(
-          "ul.ipc-metadata-list.ipc-metadata-list--dividers-all.title-pc-list.ipc-metadata-list--baseAlt li:nth-child(1) div ul li:first-child a"
-        )
-          .first()
-          .text()
-          .trim() || null;
-      const imdbCasts = [];
-      imdb$(
-        "ul.ipc-metadata-list.ipc-metadata-list--dividers-all.title-pc-list.ipc-metadata-list--baseAlt li:last-child div ul li"
-      ).each((i, li) => {
-        imdbCasts.push(imdb$(li).text().trim());
-      });
-      const imdbCastsString = imdbCasts.join(", ").toString();
+    let imdbData = await axios.get(`https://imdb.bymirrorx.eu.org/title/${imdbId}`);
 
-      imdbInfo = {
-        description: imdbDescription,
-        rating: imdbRating,
-        runtime: imdbRuntime,
-        genres: imdbGenresString,
-        director: imdbDirector,
-        casts: imdbCastsString,
-      };
-    }
+    let imdbInfo = {
+      imdb: imdb,
+      description: imdbData.data.plot,
+      poster: imdbData.data.image,
+      type: imdbData.data.contentType,
+      rating: imdbData.data.rating,
+      contentRating: imdbData.data.contentRating,
+      runtime: imdbData.data.runtime,
+      released: imdbData.data.releaseDetailed,
+      genres: imdbData.data.genre,
+      top_credits: imdbData.data.top_credits,
+      images: imdbData.data.images
+    };
+
+    // if (imdb) {
+    //   const imdbPage = await gotScraping(imdb);
+    //   const imdbHtml = imdbPage.body;
+    //   const imdb$ = cheerio.load(imdbHtml);
+
+    //   const imdbDescription =
+    //     imdb$("p[data-testid='plot'] span:first-child").text().trim() || null;
+    //   const imdbRating =
+    //     imdb$("div[data-testid='hero-rating-bar__aggregate-rating__score']")
+    //       .first()
+    //       .text()
+    //       .split("/")[0]
+    //       .trim() || null;
+    //   const imdbRuntime =
+    //     imdb$("div h1[data-testid='hero__pageTitle']")
+    //       .closest("div")
+    //       .find("ul li:last-child")
+    //       .first()
+    //       .text()
+    //       .trim() || null;
+    //   const imdbGenres = [];
+    //   imdb$("div[data-testid='genres'] div:nth-child(2) a span").each(
+    //     (i, span) => {
+    //       imdbGenres.push(imdb$(span).text().trim());
+    //     }
+    //   );
+    //   const imdbGenresString = imdbGenres.join(", ").toString();
+    //   const imdbDirector =
+    //     imdb$(
+    //       "ul.ipc-metadata-list.ipc-metadata-list--dividers-all.title-pc-list.ipc-metadata-list--baseAlt li:nth-child(1) div ul li:first-child a"
+    //     )
+    //       .first()
+    //       .text()
+    //       .trim() || null;
+    //   const imdbCasts = [];
+    //   imdb$(
+    //     "ul.ipc-metadata-list.ipc-metadata-list--dividers-all.title-pc-list.ipc-metadata-list--baseAlt li:last-child div ul li"
+    //   ).each((i, li) => {
+    //     imdbCasts.push(imdb$(li).text().trim());
+    //   });
+    //   const imdbCastsString = imdbCasts.join(", ").toString();
+
+    //   imdbInfo = {
+    //     description: imdbDescription,
+    //     rating: imdbRating,
+    //     runtime: imdbRuntime,
+    //     genres: imdbGenresString,
+    //     director: imdbDirector,
+    //     casts: imdbCastsString,
+    //   };
+    // }
 
     info = {
       title,
-      year,
-      poster,
-      imdb,
       imdbInfo,
     };
     results["info"] = info;
@@ -113,17 +133,17 @@ router.get("/subtitles/:path", async (req, res) => {
       if (td.length > 0) {
         let language =
           td
-            .find("span.l.r.neutral-icon, .l.r.positive-icon, .l.r.bad-icon")
-            .text()
-            .trim() || null;
+          .find("span.l.r.neutral-icon, .l.r.positive-icon, .l.r.bad-icon")
+          .text()
+          .trim() || null;
         let filmTitle =
           td
-            .find(
-              "span:not(.l.r.positive-icon), span:not(.l.r.neutral-icon), span:not(.l.r.bad-icon)"
-            )
-            .text()
-            .replace(language, "")
-            .trim() || null;
+          .find(
+            "span:not(.l.r.positive-icon), span:not(.l.r.neutral-icon), span:not(.l.r.bad-icon)"
+          )
+          .text()
+          .replace(language, "")
+          .trim() || null;
         let path = td.attr("href") || null;
         let author = authorLink.text().trim() || null;
         let authorProfile = baseUrl + authorLink.attr("href") || null;
